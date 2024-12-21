f242e778e744f6fdcb7d2f76ecd0dc60e30c0693
sheepmmsheep <96282436+sheepmmsheep@users.noreply.github.com>
2024-12-21 20:45:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,31 +4,25 @@ from ultralytics import YOLO
 from streamlit_webrtc import webrtc_streamer, WebRtcMode
 import av
 import numpy as np
-
-# 加载 YOLO 模型
-model_path = "best.pt"  # 替换为你的模型路径
-model = YOLO(model_path)
-
-# Streamlit 页面标题
+device = 'cpu'
+if not hasattr(st, 'obb'):
+    st.model = torch.hub.load('ultralytics/yolov11', 'custom', path="best.pt", _verbose=False)
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
 st.title("YOLO 实时追踪 with YOLO.track")
 st.sidebar.title("操作选项")
 
-# 信心阈值和 IOU 阈值
-conf_threshold = st.sidebar.slider("设置信心阈值", min_value=0.0, max_value=1.0, value=0.35, step=0.05)
-iou_threshold = st.sidebar.slider("设置IOU阈值", min_value=0.0, max_value=1.0, value=0.3, step=0.05)
+conf_threshold = st.sidebar.slider("設置信心閥值", min_value=0.0, max_value=1.0, value=0.35, step=0.05)
+iou_threshold = st.sidebar.slider("設置IOU閥值", min_value=0.0, max_value=1.0, value=0.3, step=0.05)
 
-# 定义视频处理函数
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    """处理摄像头的每一帧影像"""
-    img = frame.to_ndarray(format="bgr24")  # 转换为 OpenCV 格式
+    img = frame.to_ndarray(format="bgr24")  
 
-    # 使用 YOLO.track 进行跟踪
-    results = model.track(source=img, conf=conf_threshold, iou=iou_threshold, persist=True)
+    results = st.model.track(source=img, conf=conf_threshold, iou=iou_threshold, persist=True)
 
-    # 绘制检测框
     annotated_img = results[0].plot()
 
-    # 返回带有检测框的影像
     return av.VideoFrame.from_ndarray(annotated_img, format="bgr24")
 
 # 启动 WebRTC
